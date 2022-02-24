beb9c7dbff7f50a5aa77303deb3f8c05dd8ae7aa
Elena-Amelia <amelia013@ya.ru>
2022-02-24 22:13:41 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
-}
+    if (n < 0) {
+        n = Math.abs(n);
+    }
+        let str = n.toString();
+        let i = str.length-1;
+        let result = '';
+        while (i>=0) {
+          result = result + str[i];
+          i = i - 1;
+        }
+        return result;
+      }
+ 
